d9b653cea9d0026b6336655ecb9bf06be5dd9a9d
Arkadiusz Szłapa <arkadiusz.szlapa@ing.com>
2024-07-28 16:49:14 +0200

    fix: change deps

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
-const rateLimit = require("express-rate-limit");
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
+const rateLimiter = require("express-rate-limit");
 
 app.set("trust proxy", 1);
-app.use(limiter);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
-app.use(helmet);
-app.use(cors);
-app.use(xss);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // router
 app.use("/api/v1/jobs", jobsRouter);
